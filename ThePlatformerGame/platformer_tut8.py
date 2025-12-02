14dd619d4e2561a41bae3337078d09436f7f1303
Jedidiah-1234 <Jedidiahdaly1@gmail.com>
2025-12-02 13:24:37 +0000

    Fix Enemy Aspect Ratio/Size

--- ThePlatformerGame/platformer_tut8.py
+++ ThePlatformerGame/platformer_tut8.py
@@ -247,11 +247,10 @@ class Enemy(pygame.sprite.Sprite):
 	def __init__(self, x, y):
 		pygame.sprite.Sprite.__init__(self)
 		img = pygame.image.load('img/blob.png')
-		self.image = pygame.transform.scale(img, (tile_size, tile_size))
+		self.image = pygame.transform.scale(img, (tile_size, tile_size*0.6))
 		self.rect = self.image.get_rect()
 		self.rect.x = x
-
-		self.rect.y = y-(tile_size/3)
+		self.rect.y = y
 		
 		self.move_direction = 1
 		self.move_counter = 0
